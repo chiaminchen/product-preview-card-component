f9e7af7f99ddd0733fc9b98da618fd2f3973c979
ChiaMin <chen.chiamin@gmail.com>
2026-04-18 14:32:12 +0800

    refactor: update CSS variables for improved readability and consistency in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './components/ProductImage';
 import CartButton from './components/CartButton';
 import ProductImage from './components/ProductImage';
 
@@ -7,8 +6,8 @@ function App() {
     <main className="mx-card-margin my-20 grid max-w-150 grid-cols-1 overflow-hidden rounded-xl bg-white md:m-0 md:w-150 md:grid-cols-2">
       <ProductImage />
       <div className="flex flex-col gap-6 p-8">
-        <p className="text-xs/1.2 tracking-[0.3125rem]">PERFUME</p>
-        <h1 className="font-fraunces text-[2rem] font-bold text-black">
+        <p className="text-xs/1.2 tracking-widest">PERFUME</p>
+        <h1 className="font-fraunces text-title font-bold text-black">
           Gabrielle Essence Eau De Parfum
         </h1>
         <p className="text-sm/1.6">
@@ -16,10 +15,12 @@ function App() {
           Polge, Perfumer-Creator for the House of CHANEL.
         </p>
         <div className="flex items-center gap-4">
-          <span className="font-fraunces text-green-light text-[2rem] font-bold">
+          <p className="sr-only">Current price:</p>
+          <span className="font-fraunces text-green-light text-title font-bold">
             $149.99
           </span>
-          <span className="text-[0.8125rem]/1.2 line-through">$169.99</span>
+          <p className="sr-only">Original price:</p>
+          <span className="text-price-old/1.2 line-through">$169.99</span>
         </div>
         <CartButton />
       </div>
