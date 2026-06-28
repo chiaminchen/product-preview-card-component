f7de96131ed3cfa41363ad18fe7e682986a617bb
ChiaMin Chen <chen.chiamin@gmail.com>
2026-06-28 09:57:11 +0800

    feat: enhance responsive layout

--- src/components/ProductImage/ProductImage.jsx
+++ src/components/ProductImage/ProductImage.jsx
@@ -1,9 +1,11 @@
 import styles from './ProductImage.module.css';
 import mobileImage from '../../assets/images/image-product-mobile.jpg';
+import desktopImage from '../../assets/images/image-product-desktop.jpg';
 
 function ProductImage() {
   return (
     <picture>
+      <source media="(min-width: 768px)" srcSet={desktopImage} />
       <img
         className={styles.productImage}
         src={mobileImage}
